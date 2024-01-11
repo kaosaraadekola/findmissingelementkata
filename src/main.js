4f24b379e118ff2d45fb7d807f7772cd209011bc
Kaosara Adekola <kaosaraidris@hotmail.co.uk>
2024-01-11 14:28:41 +0000

    yarn test failing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import { sum } from "./sum.js";
+import { findMissingElements } from "./sum.js";
 
-console.log("sum is ", sum(10, 20));
-console.log("This is main.js.");
-console.log("You should probably use `yarn test` or `yarn test --watchAll`");
+
--- src/sum.js
+++ src/sum.js
@@ -1,10 +1,21 @@
-/** Adds two given numbers, returning the sum.
- * @param {number} a
- * @param {number} b
- * @returns {number} the sum of a and b
- */
-function sum(a, b) {
-    return a + b;
+let arr1 = ["Kaosara", "Lauren"]
+let arr2 = ["Kaosara"]
+
+function findMissingElements (arr1, arr2) {
+
+    let newArray = []
+
+    for (let i = 0; i < arr1.length; i++)
+
+    for (let j = 0; j < arr2.length; j++)
+
+if (arr1 [i] != arr2 [j]) {
+
+    newArray.push (arr1 [i])
+}
+return newArray;
 }
 
-export { sum };
+findMissingElements (arr1, arr2)
+
+export { findMissingElements};
